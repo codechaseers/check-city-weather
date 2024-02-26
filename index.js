68133e0e0f6cb7639dd4f7533c24724367bf4d10
sudhanshu shekhar ojha <sudhanshu@gmail.com>
2024-02-26 09:44:23 +0530

    index js add

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const api_key = "1210254713360736499061f17ac801b0";
-async function getWeather(cityName) {
+export async function getWeather(cityName) {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${ cityName}&appid=${api_key}&units=metric`;
     // weather.innerHTML = `<h2> Loading... <h2>`
 
